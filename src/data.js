251d9287777cf2ca5a088ff84ea7b593857ccd44
Jihoon Oh <jihoonoh123@gmail.com>
2019-12-08 01:04:32 -0500

    added backend

--- src/data.js
+++ src/data.js
@@ -1,50 +1,15 @@
-export default [
+// /backend/data.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure 
+const DataSchema = new Schema(
   {
-    pics: [
-      "https://images.unsplash.com/photo-1548681528-6a5c45b66b42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-    ],
-    name: "Heather",
-    age: 32,
-    distance: "4 miles away",
-    text:
-      "My nickname is Gilette because I'm the best a man can get. Also I will cut you."
+    id: Number,
+    message: String
   },
-  {
-    pics: [
-      "https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-    ],
-    name: "Chloe",
-    age: 18,
-    distance: "1 mile away",
-    text: "The C and the L are silent."
-  },
-  {
-    pics: [
-      "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    ],
-    name: "Helen",
-    age: 19,
-    distance: "3 miles away",
-    text: "Aspiring MILF"
-  },
-  {
-    pics: [
-      "https://images.unsplash.com/photo-1548247416-ec66f4900b2e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    ],
-    name: "Savannah",
-    age: 29,
-    distance: "3 miles away",
-    text: "A little known fact is that I cover about 40% of Africa."
-  },
-  {
-    pics: [
-      "https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
-      "https://images.unsplash.com/photo-1536590158209-e9d615d525e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    ],
-    name: "Jane",
-    age: 22,
-    distance: "2 miles away",
-    text:
-      "On the first date I will carve our initials in a tree. It's the most romantic way to let you know I have a knife."
-  }
-];
+  { timestamps: true }
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("Data", DataSchema);
