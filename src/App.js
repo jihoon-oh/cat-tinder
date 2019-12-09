5c78a6dda7af7d9c7bfb9b8864d4a734e6165fd5
Jihoon Oh <jihoonoh123@gmail.com>
2019-12-08 23:53:13 -0500

    finished connecting front to back, CSS needs work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from 'axios';
 import Deck from "./components/Deck";
+import UploadButton from "./components/UploadButton";
 
 class App extends Component {
 	// initialize our state
@@ -68,7 +69,7 @@ class App extends Component {
     parseInt(idTodelete);
     let objIdToDelete = null;
     this.state.data.forEach((dat) => {
-      if (dat.id === idTodelete) {
+      if (dat.id == idTodelete) {
         objIdToDelete = dat._id;
       }
     });
@@ -86,7 +87,7 @@ class App extends Component {
     let objIdToUpdate = null;
     parseInt(idToUpdate);
     this.state.data.forEach((dat) => {
-      if (dat.id === idToUpdate) {
+      if (dat.id == idToUpdate) {
         objIdToUpdate = dat._id;
       }
     });
@@ -98,73 +99,76 @@ class App extends Component {
   };
 
 
-	// render() {
-	// 	return (
-	// 		<Deck />
-	// 	);
-	// }
-
 	render() {
-	    const { data } = this.state;
-	    return (
-	      <div>
-	        <ul>
-	          {data.length <= 0
-	            ? 'NO DB ENTRIES YET'
-	            : data.map((dat) => (
-	                <li style={{ padding: '10px' }} key={data.message}>
-	                  <span style={{ color: 'gray' }}> id: </span> {dat.id} <br />
-	                  <span style={{ color: 'gray' }}> data: </span>
-	                  {dat.message}
-	                </li>
-	              ))}
-	        </ul>
-	        <div style={{ padding: '10px' }}>
-	          <input
-	            type="text"
-	            onChange={(e) => this.setState({ message: e.target.value })}
-	            placeholder="add something in the database"
-	            style={{ width: '200px' }}
-	          />
-	          <button onClick={() => this.putDataToDB(this.state.message)}>
-	            ADD
-	          </button>
-	        </div>
-	        <div style={{ padding: '10px' }}>
-	          <input
-	            type="text"
-	            style={{ width: '200px' }}
-	            onChange={(e) => this.setState({ idToDelete: e.target.value })}
-	            placeholder="put id of item to delete here"
-	          />
-	          <button onClick={() => this.deleteFromDB(this.state.idToDelete)}>
-	            DELETE
-	          </button>
-	        </div>
-	        <div style={{ padding: '10px' }}>
-	          <input
-	            type="text"
-	            style={{ width: '200px' }}
-	            onChange={(e) => this.setState({ idToUpdate: e.target.value })}
-	            placeholder="id of item to update here"
-	          />
-	          <input
-	            type="text"
-	            style={{ width: '200px' }}
-	            onChange={(e) => this.setState({ updateToApply: e.target.value })}
-	            placeholder="put new value of the item here"
-	          />
-	          <button
-	            onClick={() =>
-	              this.updateDB(this.state.idToUpdate, this.state.updateToApply)
-	            }
-	          >
-	            UPDATE
-	          </button>
-	        </div>
-	      </div>
-	    );
+		return (
+			<div>
+				<Deck />
+				<UploadButton />
+			</div>
+		);
 	}
+
+	// render() {
+	//     const { data } = this.state;
+	//     return (
+	//       <div>
+	//         <ul>
+	//           {data.length <= 0
+	//             ? 'NO DB ENTRIES YET'
+	//             : data.map((dat) => (
+	//                 <li style={{ padding: '10px' }} key={data.message}>
+	//                   <span style={{ color: 'gray' }}> id: </span> {dat.id} <br />
+	//                   <span style={{ color: 'gray' }}> data: </span>
+	//                   {dat.message}
+	//                 </li>
+	//               ))}
+	//         </ul>
+	//         <div style={{ padding: '10px' }}>
+	//           <input
+	//             type="text"
+	//             onChange={(e) => this.setState({ message: e.target.value })}
+	//             placeholder="add something in the database"
+	//             style={{ width: '200px' }}
+	//           />
+	//           <button onClick={() => this.putDataToDB(this.state.message)}>
+	//             ADD
+	//           </button>
+	//         </div>
+	//         <div style={{ padding: '10px' }}>
+	//           <input
+	//             type="text"
+	//             style={{ width: '200px' }}
+	//             onChange={(e) => this.setState({ idToDelete: e.target.value })}
+	//             placeholder="put id of item to delete here"
+	//           />
+	//           <button onClick={() => this.deleteFromDB(this.state.idToDelete)}>
+	//             DELETE
+	//           </button>
+	//         </div>
+	//         <div style={{ padding: '10px' }}>
+	//           <input
+	//             type="text"
+	//             style={{ width: '200px' }}
+	//             onChange={(e) => this.setState({ idToUpdate: e.target.value })}
+	//             placeholder="id of item to update here"
+	//           />
+	//           <input
+	//             type="text"
+	//             style={{ width: '200px' }}
+	//             onChange={(e) => this.setState({ updateToApply: e.target.value })}
+	//             placeholder="put new value of the item here"
+	//           />
+	//           <button
+	//             onClick={() =>
+	//               this.updateDB(this.state.idToUpdate, this.state.updateToApply)
+	//             }
+	//           >
+	//             UPDATE
+	//           </button>
+	//         </div>
+	//       </div>
+	//     );
+	// }
 }
 
 export default App;
---
+++ src/components/UploadButton.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class UploadButton extends Component {
+	render() {
+		return (
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+				</div>
+				<div class="custom-file">
+					<input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01" />
+			    	<label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default UploadButton;
